8ea66ba31764d117faa4f37cb6589bbe558f4e83
unknown <kartikshirale1230@gmail.com>
2023-12-04 14:32:08 +0530

    fix errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,7 +7,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 
@@ -73,7 +73,10 @@ export async function GET() {
       })
     );
 
-    return updatedProducts
+    return NextResponse.json({
+      message: "Ok",
+      data: updatedProducts,
+    });
 
   } catch (error: any) {
     throw new Error(`Failed to get all products: ${error.message}`);
